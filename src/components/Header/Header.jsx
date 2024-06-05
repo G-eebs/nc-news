075ee8bfdcec80135567707db5c6d80e5688a0dd
George B <george.bartram29@gmail.com>
2024-06-05 16:27:53 +0100

    adds colour change to selected page in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Header.css";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../contexts/User";
@@ -19,17 +19,19 @@ function Header() {
 			});
 	}, []);
 
+	const currentLocation = useLocation().pathname
+
 	return (
 		<header className="header">
 			<h1 className="title">{"NC\nNews"}</h1>
 			<nav className="nav-bar">
-				<Link to="/" className="nav-link">
+				<Link to="/" className={"nav-link" + (currentLocation === "/" ? " nav-link-current" : "")}>
 					Home
 				</Link>
-				<Link to="/articles" className="nav-link">
+				<Link to="/articles" className={"nav-link" + (currentLocation === "/articles" ? " nav-link-current" : "")}>
 					Articles
 				</Link>
-				<Link to="/post-article" className="nav-link">
+				<Link to="/post-article" className={"nav-link" + (currentLocation === "/post-article" ? " nav-link-current" : "")}>
 					{"Post Article"}
 				</Link>
 				{error ? (
