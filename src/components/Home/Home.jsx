63393591052a9978ec3dbf9dd8bbaecdaabdf187
George B <george.bartram29@gmail.com>
2024-06-06 01:59:39 +0100

    adds functioning home page with links

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,60 @@
+import { useEffect, useState } from "react";
+import "./Home.css"
+import { getArticles } from "../../utils/util-api-calls";
+import ErrorComponent from "../ErrorComponent/ErrorComponent";
+import ArticleCard from "../ArticleCard/ArticleCard";
+import { Link } from "react-router-dom";
+import { capitaliseFirstLetter } from "../../utils/util-data-formatters";
+
 function Home() {
-  return <h2>Home (Placeholder, try "Articles")</h2>
+	const [popularArticles, setPopularArticles] = useState([]);
+	const [articlesLoading, setArticlesLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		setArticlesLoading(true);
+		setError(null);
+		getArticles( {params: {sortBy: "votes", order: "desc"}})
+			.then((res) => {
+				setArticlesLoading(false);
+        const allArticles = res.data.articles
+        const coveredTopics = []
+        const topArticleEachTopic = allArticles.filter((article)=> {
+          if(!coveredTopics.includes(article.topic)) {
+            coveredTopics.push(article.topic)
+            return article
+          }
+        })
+				setPopularArticles(topArticleEachTopic);
+			})
+			.catch((err) => {
+				setArticlesLoading(false);
+				setError(err.response);
+			});
+	}, []);
+
+	return (
+		<>
+			{articlesLoading ? (
+				<h2>Loading ...</h2>
+			) : error ? (
+				<ErrorComponent status={error.status} message={error.data.msg} />
+			) : (
+				<>
+					<h2 className="home-title">Popular Articles</h2>
+          {popularArticles.map(article => {
+            return (<div className="popular-topic">
+            <Link to={`/articles?topic=${article.topic}`} className="popular-topic-title">{capitaliseFirstLetter(article.topic)}</Link>
+						<ArticleCard
+							articleData={article}
+							key={article.article_id}
+						/>
+					</div>)
+          })}
+				</>
+			)}
+		</>
+	);
 }
 
-export default Home
+export default Home;
